a44062c6b8d1a13458789a64ae8f70f112949fd9
ambarpalermo <ambarpalermo06@gmail.com>
2022-03-29 08:41:25 -0300

    ejercicio3

---
+++ Assets/ej03.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ej03 : MonoBehaviour
+{
+    public string txt = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola " + txt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
